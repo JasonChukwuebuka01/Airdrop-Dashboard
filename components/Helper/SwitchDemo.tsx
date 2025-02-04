d2a89908d7db6fd1059f955e1fcbbc6b1e6d5945
jason <jasonchinonso20@gmail.com>
2025-02-04 22:40:50 +0100

    Modified

---
+++ components/Helper/SwitchDemo.tsx
@@ -0,0 +1,10 @@
+
+import { Switch } from "@/components/ui/switch"
+
+export default function SwitchDemo() {
+  return (
+    <div className="flex items-center space-x-2 ">
+      <Switch id="airplane-mode"  />
+    </div>
+  )
+}
--- components/ShowTable.tsx
+++ components/ShowTable.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import DataTable from './Helper/Table'
+import DataTable from './Helper/dataTable'
+import SwitchDemo from './Helper/SwitchDemo'
 
 const ShowTable = () => {
 
     return (
-        <section className='border-2 border-red-500'>
-            <header className='flex justify-between items-center '>
-                <h2>Speedtest Statistics</h2>
+        <section className='border-2 border-red-500 p-2 rounded-lg'>
+            <header className='flex justify-between items-center border-2 border-red-500'>
+                <h2 className='text-base lg:text-xl font-bold text-gray-800 mb-2 '>Speedtest Statistics</h2>
 
-                <article className='flex gap-2'>
-                    <span></span>
-                    <h2>Hide IP Address</h2>
+                <article className='flex gap-2 justify-center items-center'>
+                    <div className=''>
+                        <SwitchDemo />
+                    </div>
+                    <h2 className='text-base lg:text-lg font-bold text-gray-800 text-center'>Hide IP Address</h2>
                 </article>
             </header>
 
