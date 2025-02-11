5ba381ddc975d3f52c55f628d685f36176841522
jason <jasonchinonso20@gmail.com>
2025-02-11 08:12:20 +0100

    Modified

--- components/Helper/DatePicker.tsx
+++ components/Helper/DatePicker.tsx
@@ -24,12 +24,12 @@ export function DatePickerDemo() {
         <Button
           variant={"outline"}
           className={cn(
-            "w-full justify-start text-left font-normal",
+            "w-full lg:w-[280px] justify-start text-left font-normal",
             !date && "text-muted-foreground"
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          {date ? format(date, "PPP") : <span>Pick aa date</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
