61137d56e21999ddd696f0cfd4ed70865fadf40a
jason <jasonchinonso20@gmail.com>
2025-02-22 00:30:55 +0100

    Modified

--- components/Helper/QuestReward.tsx
+++ components/Helper/QuestReward.tsx
@@ -17,7 +17,7 @@ const QuestsRewards = () => {
     const [isClaimable, setIsClaimable] = useState<boolean>(false);
     const [isWeeklyClaimableButton, setIsWeeklyClaimableButton] = useState<string>(localStorage.getItem("claimableButton") || "false");
     const [weeklyButtonValue, setWeeklyButtonValue] = useState<number>(1000);
-    const [streak, setStreak] = useState<number>(8);
+    const [streak, setStreak] = useState<number>(28);
     const [claimed, setClaimed] = useState<boolean>(false);
     const [totalEarned, setTotalEarned] = useState<number>(0);
     const [questsCompleted, setQuestsCompleted] = useState<number>(0);
